f819139b2ca0073859919748e54cb15498bf2905
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-19 12:21:58 -0400

    Création du contrôleur permettant de servir la vue d'angular

---
+++ app/Http/Controllers/AngularController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AngularController extends Controller {
+    // Servir la vue renfermant le déploiment d'Angular
+    public function index() {
+        return view("angular");
+    }
+}
