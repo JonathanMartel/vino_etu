adf4661ceac1c599ff471b95de8103f638096dd4
jordan mirarchi <mirarchi04@gmail.com>
2021-10-20 20:14:13 -0400

    ajout du component accueil

--- resources/frontend/src/app/app-routing.module.ts
+++ resources/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
-
+    {path:"", component:AccueilComponent},
 ];
 
 @NgModule({
