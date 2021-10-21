f6153d3205dc68f0155592c5f9b86b697996a843
Pascal Beausoleil <beausoleil.pascal@gmail.com>
2021-10-21 12:55:52 -0400

    Ajout du component BouteilleCellier

---
+++ client/src/components/BouteilleCellier/BouteilleCellier.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './BouteilleCellier.css';
+
+class extends BouteilleCellier extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {  }
+	}
+	
+	
+	
+	
+	render() { 
+		
+		
+		
+		
+		return (  );
+	}
+}
