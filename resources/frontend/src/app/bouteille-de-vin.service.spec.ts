a8c6f01de464384e836563c94b1ac5c8fba69aaf
jordan mirarchi <mirarchi04@gmail.com>
2021-10-20 20:18:08 -0400

    creation du service bouteilleDeVin

---
+++ resources/frontend/src/app/bouteille-de-vin.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BouteilleDeVinService } from './bouteille-de-vin.service';
+
+describe('BouteilleDeVinService', () => {
+  let service: BouteilleDeVinService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BouteilleDeVinService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
