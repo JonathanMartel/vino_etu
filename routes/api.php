26b2ffa464b2887959cdcfad0f2ccfc50237dc5f
jordan mirarchi <mirarchi04@gmail.com>
2021-10-20 21:01:11 -0400

    creation du model vino bouteille et creation dune route vers api/bouteilles

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\vino__bouteille;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/bouteilles', function(){
+
+    return vino__bouteille::all();
 });
