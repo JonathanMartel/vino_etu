6d162e8c19bad5c0b7772e30f07fad148a4ae6a9
Brian Tan <t.brian3@gmail.com>
2021-10-22 19:06:43 -0400

    rating d'une bouteille

--- resources/views/cellierBouteille/create.blade.php
+++ resources/views/cellierBouteille/create.blade.php
@@ -24,11 +24,12 @@
           <label for="quantite">Quantité</label>
         </div>
         <div class="input-field col s12">
-          <input id="quantite" type="number" min="1" class="validate">
-          <label for="quantite">Date d'achat</label>
+          <input id="quantite" type="number" pattern="[0-9]+(\\.[0-9][0-9]?)?" step=".01" class="validate">
+          <span class="helper-text" data-error="Format invalid"></span>
+          <label for="quantite">Prix</label>
         </div>
         <div class="input-field col s12">
-          <input id="quantite" type="number" min="1" class="validate">
+          <input id="quantite" type="number" class="validate">
           <label for="quantite">Millesime</label>
         </div>
         <div class="input-field col s12">
@@ -43,9 +44,21 @@
           <input id="garde_jusqua" type="text" class="validate">
           <label for="garde_jusqua">Garder jusqu'à</label>
         </div>
+        <div class="rate">
+          <input type="radio" id="star5" name="rate" value="5" />
+          <label for="star5" title="text">5 stars</label>
+          <input type="radio" id="star4" name="rate" value="4" />
+          <label for="star4" title="text">4 stars</label>
+          <input type="radio" id="star3" name="rate" value="3" />
+          <label for="star3" title="text">3 stars</label>
+          <input type="radio" id="star2" name="rate" value="2" />
+          <label for="star2" title="text">2 stars</label>
+          <input type="radio" id="star1" name="rate" value="1" />
+          <label for="star1" title="text">1 star</label>
+        </div>
         <input type="hidden" id="bouteille_id">
     </form>
     </div>
 @endsection
-
+<link href="{{asset('css/rating.css')}}" rel="stylesheet" />
 <script src="{{asset('js/cellierBouteille_create.js')}}"></script>
