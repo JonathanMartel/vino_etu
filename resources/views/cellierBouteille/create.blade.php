3874097df6ece18225ae1578f84c01efe1462078
Brian Tan <t.brian3@gmail.com>
2021-10-22 16:47:24 -0400

    Liste des modèles d'opération

--- resources/views/cellierBouteille/create.blade.php
+++ resources/views/cellierBouteille/create.blade.php
@@ -7,30 +7,45 @@
         <div class="input-field col s12">
           <i class="material-icons prefix">search</i>
           <input type="text" name="autocomplete" autocomplete="off">
-          <label for="autocomplete">Recherche</label>
+          <label for="autocomplete">Rechercher une bouteille</label>
           <ul></ul>
         </div>
       </div>
     </div>
   </div>
 <div class="row">
-    <form class="col s12">
-        <div class="input-field col s6">
+    <form class="col s12 z-depth-5">
+        <div class="input-field col s12">
           <input id="nom" type="text" class="validate">
           <label for="nom">Nom</label>
         </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
+        <div class="input-field col s12">
+          <input id="quantite" type="number" min="1" class="validate">
+          <label for="quantite">Quantité</label>
         </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
+        <div class="input-field col s12">
+          <input id="quantite" type="number" min="1" class="validate">
+          <label for="quantite">Date d'achat</label>
         </div>
-      </div>
-  
+        <div class="input-field col s12">
+          <input id="quantite" type="number" min="1" class="validate">
+          <label for="quantite">Millesime</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="date_achat" type="text" class="datepicker validate">
+          <label for="date_achat">Date d'achat</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="commentaire" type="text" class="validate">
+          <label for="commentaire">Commentaire</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="garde_jusqua" type="text" class="validate">
+          <label for="garde_jusqua">Garder jusqu'à</label>
+        </div>
+        <input type="hidden" id="bouteille_id">
     </form>
- 
+    </div>
 @endsection
 
 <script src="{{asset('js/cellierBouteille_create.js')}}"></script>
