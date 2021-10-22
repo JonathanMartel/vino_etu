c94351ed0823007d9cd1f147e03835eacb1f0ab6
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-22 16:26:27 -0400

    Modification du modèle User pour accepter plus de champs

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'city',
+        'dob',
         'email',
         'password',
     ];
