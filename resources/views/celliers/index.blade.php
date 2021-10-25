fa999805d7ebe75e8160a5fa72f8a173afea77b6
Niwark <blackaru@hotmail.com>
2021-10-25 17:12:11 -0400

    debut css liste celliers

--- resources/views/celliers/index.blade.php
+++ resources/views/celliers/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
+<link href="{{asset('css/celliers.css')}}" rel="stylesheet" />
+
 <div class="col-8">
     <h1>Vos celliers</h1>
     <p>Pour entrer dans un cellier, veuiller cliquer sur sa vignette</p>
@@ -10,22 +12,24 @@
     <a href="/create/cellier">Ajouter un cellier</a>
 </div>
 
-
+<div class="liste-cellier">
 @forelse($celliers as $cellier)
 <a href="./cellier/{{ $cellier->id }}">
-    <article class="flex-row">
+    <article class="cellier">
         <div>
-            <h2>{{ ucfirst($cellier->nom) }}</h2>
-            <p>{{ ucfirst($cellier->localisation) }}</p>
+            <h2 class="titre-vignette">{{ ucfirst($cellier->nom) }}</h2>
+            <p class="localisation-vignette">{{ ucfirst($cellier->localisation) }}</p>
         </div>
         <img src="" alt="Logo bouteille">
-        <p>42</p><!-- !!! insérer nb vins dans cellier ici  -->
+        <p class="nb-vins-cellier">42</p><!-- !!! insérer nb vins dans cellier ici  -->
 
     </article>
 </a>
 @empty
 <p>Vous n'avez pour l'instant aucun cellier.</p>
 @endforelse
+</div>
+
 
 
 
