380d11e8f216055e6d19140f60ab5e41cb83a315
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-25 08:31:41 -0400

    Ajout d'un seed user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "first_name" => "Vino",
+            "last_name" => "Kalimotxo",
+            "city" => "Montréal",
+            "dob" => "1980-10-20",
+            "email" => "vino@kalimotxo.com",
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
