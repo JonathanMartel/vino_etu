a8642f0cc6be915f79458ebc2d40fdcb99b9920d
jordan mirarchi <mirarchi04@gmail.com>
2021-10-25 18:38:48 -0400

    1er pull request reussit :)

--- resources/frontend/src/app/app-routing.module.ts
+++ resources/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccueilComponent } from './accueil/accueil.component';
-import { CellierComponent } from './cellier/cellier.component';
-import { ListeBouteilleComponent } from './liste-bouteille/liste-bouteille.component';
-import { AjoutBouteilleComponent } from './ajout-bouteille/ajout-bouteille.component';
+import { AccueilComponent } from '@pages/accueil/accueil.component';
+import { CellierComponent } from '@pages/cellier/cellier.component';
+import { ListeBouteilleComponent } from '@pages/liste-bouteille/liste-bouteille.component';
+import { AjoutBouteilleComponent } from '@pages/ajout-bouteille/ajout-bouteille.component';
 
 const routes: Routes = [
     {path:"", component:AccueilComponent},
