f0d7df534ed9074d00f504c7d86efe6101f0e3a7
helenheshu <helen_heshu@me.com>
2021-10-26 17:13:08 -0400

    ajouter Logo, icon et modifie le font

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,5 +2,14 @@
 
 @section('content')
 
-dashboard
+{{$nom}}
+
+{{$courriel}}
+
+{{$date_naissance}}
+<div class="row">
+    <div class="col s7 push-s5"><span class="flow-text">This div is 7-columns wide on pushed to the right by 5-columns.</span></div>
+    <div class="col s5 pull-s7"><span class="flow-text">5-columns wide pulled to the left by 7-columns.</span></div>
+</div>
+
 @endsection
