aafb454914a3b8bebd187d29f13236f51d22050c
lebobus <robertgaina@outlook.fr>
2021-10-26 19:03:15 -0400

    Changement de la fonction login pour utiliser le courriel au lieu de l'id

--- api/modeles/UsagerModele.php
+++ api/modeles/UsagerModele.php
@@ -12,16 +12,16 @@ class UsagerModele extends Modele
     /**
      * Retourne si l'authorisation HTTP match un usager à la db.
      *
-     * @param mixed $id L'id de l'usager.
+     * @param String Courriel de l'usager.
      * @param String Mot de passe de l'usager.
      * 
      * @throws Exception Erreur de requête sur la base de données.
      *  
      * @return Boolean Authentification valide.
      */
-    public function match($id, $password)
+    public function match($courriel, $password)
     {
-        $requete = "SELECT id_usager FROM vino__usager WHERE id_usager = $id AND password_usager = '$password'";
+        $requete = "SELECT  FROM vino__usager WHERE courriel = $courriel AND password_usager = '$password'";
         $match = false;
 
         if (($res = $this->_db->query($requete)) == true) {
