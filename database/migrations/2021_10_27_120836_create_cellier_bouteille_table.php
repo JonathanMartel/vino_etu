4e2d7d8144939add46513cd7e41a7823b86f8f10
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-27 08:14:52 -0400

    Mise à jour de la migration du pivot

--- database/migrations/2021_10_27_120836_create_cellier_bouteille_table.php
+++ database/migrations/2021_10_27_120836_create_cellier_bouteille_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCellierBouteilleTable extends Migration
+class CreateCelliersBouteillesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateCellierBouteilleTable extends Migration
      */
     public function up()
     {
-        Schema::create('cellier_bouteille', function (Blueprint $table) {
+        Schema::create('celliers_bouteilles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("celliers_id")->constrained();
+            $table->foreignId("bouteilles_id")->constrained();
+            $table->smallInteger("inventaire", false, true);
             $table->timestamps();
         });
     }
