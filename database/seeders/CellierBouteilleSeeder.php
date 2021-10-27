040e0092b723410ce6a83baf0f037a0ab4a7b07a
dgtlmlta <nicolas@digitalmilitia.net>
2021-10-27 09:06:18 -0400

    Seeder du cellier bouteille

--- database/seeders/CellierBouteilleSeeder.php
+++ database/seeders/CellierBouteilleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CellierBouteille;
 use Illuminate\Database\Seeder;
 
 class CellierBouteilleSeeder extends Seeder
@@ -13,6 +14,31 @@ class CellierBouteilleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $cellierBouteilles = [
+            [
+                "cellier_id"   => 1,
+                "bouteille_id" => 45,
+                "inventaire"   => 2
+            ],
+            [
+                "cellier_id"   => 1,
+                "bouteille_id" => 78,
+                "inventaire"   => 1
+            ],
+            [
+                "cellier_id"   => 1,
+                "bouteille_id" => 2,
+                "inventaire"   => 1
+            ],
+            [
+                "cellier_id"   => 1,
+                "bouteille_id" => 136,
+                "inventaire"   => 3
+            ],
+        ];
+
+        foreach ($cellierBouteilles as $bouteille) {
+            CellierBouteille::create($bouteille);
+        }
     }
 }
