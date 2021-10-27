d1d524e5fe4625b509f9692867622b1cfde11a54
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-27 09:46:35 -0400

    Changement de nom du FK car il était trop long

--- database/migrations/2021_10_27_122136_create_celliers_bouteilles_personnalisees_table.php
+++ database/migrations/2021_10_27_122136_create_celliers_bouteilles_personnalisees_table.php
@@ -15,9 +15,11 @@ class CreateCelliersBouteillesPersonnaliseesTable extends Migration
     {
         Schema::create('celliers_bouteilles_personnalisees', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("cellier_id")->constrained();
-            $table->foreignId("bouteille_personnalisee_id")->constrained("bouteilles_personnalisees");
+            $table->foreignId("celliers_id")->constrained();
+            $table->bigInteger("bouteilles_personnalisees_id")->unsigned();
             $table->timestamps();
+
+            $table->foreign("bouteilles_personnalisees_id", "cellier_bp_fk")->references("id")->on("bouteilles_personnalisees");
         });
     }
 
