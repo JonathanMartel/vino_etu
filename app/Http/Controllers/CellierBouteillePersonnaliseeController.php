68151882611954739f6fa0354e14d1ce7b25af1e
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-27 08:22:29 -0400

    Création du modèle pivot CellierBouteillePersonnalisée

---
+++ app/Http/Controllers/CellierBouteillePersonnaliseeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CellierBouteillePersonnalisee;
+use Illuminate\Http\Request;
+
+class CellierBouteillePersonnaliseeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CellierBouteillePersonnalisee  $cellierBouteillePersonnalisee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CellierBouteillePersonnalisee $cellierBouteillePersonnalisee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CellierBouteillePersonnalisee  $cellierBouteillePersonnalisee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CellierBouteillePersonnalisee $cellierBouteillePersonnalisee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CellierBouteillePersonnalisee  $cellierBouteillePersonnalisee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CellierBouteillePersonnalisee $cellierBouteillePersonnalisee)
+    {
+        //
+    }
+}
