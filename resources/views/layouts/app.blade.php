8e0383351ad6a66cd5554d5d3f77a519dc7e9ee4
helenheshu <helen_heshu@me.com>
2021-10-28 12:01:43 -0400

    modifie entete et footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,13 +32,13 @@
 </head>
 
 <body>
-
-    <header>
+<div id="body_container">
+    <header class="flex-row padding-1rem z-depth-1">
             <!-- Voir la navigation !!! -->
+        <a href="#" class="brand-logo left "><img src="{{asset('assets/logo/logo-vino.svg')}}"></a>
 
-        <nav class="nav-extended padding-30 white">
-            <div class="nav-wrapper  height-120">
-                <a href="#" class="brand-logo left "><img src="{{asset('assets/logo/logo-vino.svg')}}"></a>
+        <nav class="nav-extended white z-depth-0">
+            <div class="nav-wrapper">
                 <a href="#" class="sidenav-trigger right" data-target="mobile-links"><i class="material-icons" style="font-size: 50px"><span class="black-text  md-48">menu</span></i></a>
                 <ul class="right hide-on-med-and-down">
                     @guest
@@ -52,7 +52,6 @@
             </div>
             <ul class="sidenav" id="mobile-links">
                 <li><a href="/cellier">Vos celliers</a></li>
-                <li><a href="/home">Vos vins</a></li>
                 @guest
                     <li><a href="/registration">S'inscrire</a></li>
                     <li><a href="/login">Se connecter</a></li>
@@ -65,7 +64,6 @@
                 <div class="col s6">
                     <ul class="tabs tabs-transparent hide-on-med-and-down">
                         <li class="tab"><a href="/cellier"><span class="black-text">Vos celliers</sapn></a></li>
-                        <li class="tab"><a href="/home"><span class="black-text">Vos vins</sapn></a></li>
                         <li class="tab"><a href="/dashboard"><span class="black-text">Compte</sapn></a></li>
                     </ul>
                 </div>
@@ -73,10 +71,10 @@
         </nav>
 
     </header>
-
+    <main>
 
     @yield('content')
-
+    </main>
     <!-- Footer -->
     <footer class="footer text-faded text-center py-5">
         <div class="container">
@@ -86,7 +84,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="{{asset('js/scripts.js')}}"></script>
     <script src="{{asset('js/app.js')}}"></script>
-
+</div>
 </body>
 
 </html>
