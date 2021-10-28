b8ac2f24c0d68496bdafd20639971fcc07bf21b4
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-10-28 11:14:31 -0400

    Ajout d'une méthode relationnelle pour lier les modèles Pays et Bouteilles

--- app/Models/Pays.php
+++ app/Models/Pays.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Pays extends Model
 {
     use HasFactory;
+
+    public function bouteilles() {
+        return $this->hasMany(Bouteille::class);
+    }
 }
