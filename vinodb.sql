5afad9d68da89a564b45fd973e68e6d9fdf12714
PascalB1982 <44015390+PascalB1982@users.noreply.github.com>
2021-10-28 14:44:11 -0400

    Modif SQL

--- vinodb.sql
+++ vinodb.sql
@@ -9,7 +9,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__type` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__type` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`id`))
@@ -17,14 +17,14 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__action` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__action` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__usager` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__usager` (
   `id_usager` INT(11) NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NULL DEFAULT NULL,
   `prenom` VARCHAR(45) NULL DEFAULT NULL,
@@ -37,7 +37,7 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COMMENT = '			';
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__cellier` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__cellier` (
   `usager_id` INT(11) NOT NULL,
   `id_cellier` INT(11) NOT NULL AUTO_INCREMENT,
   `emplacement` VARCHAR(45) NULL DEFAULT NULL,
@@ -46,14 +46,14 @@ CREATE TABLE IF NOT EXISTS `vinodb`.`vino__cellier` (
   INDEX `fk_cellier_usager_idx` (`usager_id` ASC) ,
   CONSTRAINT `fk_cellier_usager`
     FOREIGN KEY (`usager_id`)
-    REFERENCES `vinodb`.`vino__usager` (`id_usager`)
+    REFERENCES `dbs4676326`.`vino__usager` (`id_usager`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__bouteille` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__bouteille` (
   `usager_id_usager` INT(11) NOT NULL,
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(200) NULL DEFAULT NULL,
@@ -73,19 +73,19 @@ CREATE TABLE IF NOT EXISTS `vinodb`.`vino__bouteille` (
   INDEX `fk_vino__bouteille_usager1_idx` (`usager_id_usager` ASC) ,
   CONSTRAINT `fk_vino__bouteille_vino__type1`
     FOREIGN KEY (`vino__type_id`)
-    REFERENCES `vinodb`.`vino__type` (`id`)
+    REFERENCES `dbs4676326`.`vino__type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_vino__bouteille_usager1`
     FOREIGN KEY (`usager_id_usager`)
-    REFERENCES `vinodb`.`vino__usager` (`id_usager`)
+    REFERENCES `dbs4676326`.`vino__usager` (`id_usager`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 AUTO_INCREMENT = 11
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__cellier_historique` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__cellier_historique` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `id_usager` INT(11) NOT NULL,
   `id_cellier` INT(11) NOT NULL,
@@ -101,22 +101,22 @@ CREATE TABLE IF NOT EXISTS `vinodb`.`vino__cellier_historique` (
   INDEX `fk_vino__cellier_mouv_usager1_idx` (`id_usager` ASC) ,
   CONSTRAINT `fk_vino__cellier_mouv_vino__bouteille1`
     FOREIGN KEY (`bouteille_id`)
-    REFERENCES `vinodb`.`vino__bouteille` (`id`)
+    REFERENCES `dbs4676326`.`vino__bouteille` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_vino__cellier_mouv_cellier1`
     FOREIGN KEY (`id_cellier`)
-    REFERENCES `vinodb`.`vino__cellier` (`id_cellier`)
+    REFERENCES `dbs4676326`.`vino__cellier` (`id_cellier`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_vino__cellier_mouv_type_mouv1`
     FOREIGN KEY (`action_id`)
-    REFERENCES `vinodb`.`vino__action` (`id`)
+    REFERENCES `dbs4676326`.`vino__action` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_vino__cellier_mouv_usager1`
     FOREIGN KEY (`id_usager`)
-    REFERENCES `vinodb`.`vino__usager` (`id_usager`)
+    REFERENCES `dbs4676326`.`vino__usager` (`id_usager`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -124,7 +124,7 @@ AUTO_INCREMENT = 10
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__cellier_inventaire` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__cellier_inventaire` (
   `usager_id` INT(11) NOT NULL,
   `id_cellier` INT(11) NOT NULL,
   `bouteille_id` INT(11) NOT NULL,
@@ -133,18 +133,18 @@ CREATE TABLE IF NOT EXISTS `vinodb`.`vino__cellier_inventaire` (
   INDEX `fk_vino__inventaire_cellier_vino__bouteille1_idx` (`usager_id` ASC, `bouteille_id` ASC) ,
   CONSTRAINT `fk_solde_cellier_cellier1`
     FOREIGN KEY (`id_cellier`)
-    REFERENCES `vinodb`.`vino__cellier` (`id_cellier`)
+    REFERENCES `dbs4676326`.`vino__cellier` (`id_cellier`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_vino__inventaire_cellier_vino__bouteille1`
     FOREIGN KEY (`usager_id` , `bouteille_id`)
-    REFERENCES `vinodb`.`vino__bouteille` (`usager_id_usager` , `id`)
+    REFERENCES `dbs4676326`.`vino__bouteille` (`usager_id_usager` , `id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `vinodb`.`vino__bouteille_saq` (
+CREATE TABLE IF NOT EXISTS `dbs4676326`.`vino__bouteille_saq` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(200) NULL DEFAULT NULL,
   `image` VARCHAR(200) NULL DEFAULT NULL,
@@ -161,12 +161,12 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 11
 DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO `vinodb`.`vino__type` (`type`) VALUES ('vin rouge');
-INSERT INTO `vinodb`.`vino__type` (`type`) VALUES ('vin blanc');
-INSERT INTO `vinodb`.`vino__type` (`type`) VALUES ('vin rosé');
+INSERT INTO `dbs4676326`.`vino__type` (`type`) VALUES ('vin rouge');
+INSERT INTO `dbs4676326`.`vino__type` (`type`) VALUES ('vin blanc');
+INSERT INTO `dbs4676326`.`vino__type` (`type`) VALUES ('vin rosé');
 
 
-INSERT INTO `vinodb`.`vino__usager` (`prenom`, `courriel`, `est_admin`) VALUES ('Robert', 'bobus@gmail.com', '1');
+INSERT INTO `dbs4676326`.`vino__usager` (`prenom`, `courriel`, `est_admin`) VALUES ('Robert', 'bobus@gmail.com', '1');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
