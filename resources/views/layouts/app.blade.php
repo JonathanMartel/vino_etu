4befac5b6e215a2b4c9786527cb75830cb89ce6e
Pascal Lesieur <greencupandthedudes@gmail.com>
2021-10-29 16:26:52 -0400

    modification des h1, et icon d'entete, réduire leur taille

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
 <div id="body_container">
     <header class="flex-row padding-1rem z-depth-1">
             <!-- Voir la navigation !!! -->
-        <a href="{{ route('cellier') }}" class="brand-logo left "><img src="{{asset('assets/logo/logo-vino.svg')}}" alt="logo InVino"></a>
+        <a href="{{ route('cellier') }}" class="brand-logo left "><img class="logo" src="{{asset('assets/logo/logo-vino.svg')}}" alt="logo InVino"></a>
 
         <nav class="nav-extended white z-depth-0">
             <div class="nav-wrapper">
