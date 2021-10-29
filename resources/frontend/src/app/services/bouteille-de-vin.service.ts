79fea1a1170d4f3ba11ae4285ea8856ba8f7a094
dgtlmlta <nicolas@digitalmilitia.net>
2021-10-28 23:51:51 -0400

    Mise  jour css et adresse pour l'api et copie des images pour les rendre accessible dans laravel

--- resources/frontend/src/app/services/bouteille-de-vin.service.ts
+++ resources/frontend/src/app/services/bouteille-de-vin.service.ts
@@ -7,10 +7,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BouteilleDeVinService {
 
-    private url:string = "http://127.0.0.1:8000/api";
-    // private url: string = "http://e2095388.webdev.cmaisonneuve.qc.ca/api";
+    // private url:string = "http://127.0.0.1:8000/api";
+    private url: string = new URL(window.location.href).origin + "/api";
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        console.log(this.url);
+     }
 
     getCellier() {
 
