2b8ab19250054af54649fc794896587918f80e39
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-01 13:35:39 -0400

    Création de la migration pour la table BouteillesAchetees

--- database/migrations/2021_11_01_171945_create_bouteilles_achetees_table.php
+++ database/migrations/2021_11_01_171945_create_bouteilles_achetees_table.php
@@ -15,6 +15,19 @@ class CreateBouteillesAcheteesTable extends Migration
     {
         Schema::create('bouteilles_achetees', function (Blueprint $table) {
             $table->id();
+            $table->string("nom");
+            $table->text("description")->nullable();
+            $table->string("url_image")->nullable();
+            $table->string("url_achat")->nullable();
+            $table->string("url_info")->nullable();
+            $table->string("origine");
+            $table->smallInteger("millesime")->unsigned();
+            $table->date("date_acquisition")->nullable();
+            $table->float("prix_paye", 8, 2, true)->nullable();
+            $table->string("conservation", 500)->nullable();
+            $table->text("notes_personnelles")->nullable();
+            $table->string("format")->nullable();
+            $table->foreignId("categories_id")->constrained("categories");
             $table->timestamps();
         });
     }
