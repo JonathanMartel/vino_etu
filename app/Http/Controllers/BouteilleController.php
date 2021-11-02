a295a0f302cba9fd0804e2635d249cd10d7d10cb
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-02 10:16:45 -0400

    premiers tests de full text search dans le contrôleur du catalogue

--- app/Http/Controllers/BouteilleController.php
+++ app/Http/Controllers/BouteilleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\BouteilleResource;
 use App\Models\Bouteille;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BouteilleController extends Controller {
     /**
@@ -17,12 +18,23 @@ class BouteilleController extends Controller {
 
         $request->orderBy = "nom";
         $request->orderDirection = "asc";
+        $request->texteRecherche = "États";
 
         /* return BouteilleResource::collection(
             Bouteille::orderBy($request->orderBy, $request->orderDirection)
                      ->paginate($request->limite)); */
-        if($request->texteRecherche) {
-            Bouteille::
+        if($recherche = $request->texteRecherche) {
+            $sousReqPays = DB::table("pays")
+                             ->select("id", "nom")
+                             ->whereRaw("MATCH(nom) against (? in natural language mode)", [$recherche]);
+
+            $requete = DB::table("bouteilles")
+                         ->joinSub($sousReqPays, "pays", function($join) {
+                            $join->on("pays.id", "=", "bouteilles.pays_id");
+                        })
+                        ->paginate($request->limite);
+
+            return $requete;
         }
     }
 
