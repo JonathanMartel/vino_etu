c0a912e27a26aad711db060ecc109de27f5be2e7
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-02 16:47:00 -0400

    Travail sur le changement d'inventaire à compléter, prévenir le call http si la valeur est inchangée

--- resources/frontend/src/app/components/cellier-bouteille/cellier-bouteille.component.ts
+++ resources/frontend/src/app/components/cellier-bouteille/cellier-bouteille.component.ts
@@ -1,77 +1,92 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { startWith, pairwise} from "rxjs/operators"
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ModifierCellierBouteilleComponent } from '@pages/modifier-cellier-bouteille/modifier-cellier-bouteille.component';
 import { BouteilleDeVinService } from '@services/bouteille-de-vin.service';
 
 @Component({
-  selector: 'app-cellier-bouteille',
-  templateUrl: './cellier-bouteille.component.html',
-  styleUrls: ['./cellier-bouteille.component.scss']
+    selector: 'app-cellier-bouteille',
+    templateUrl: './cellier-bouteille.component.html',
+    styleUrls: ['./cellier-bouteille.component.scss']
 })
 export class CellierBouteilleComponent implements OnInit {
 
-  bouteille: any;
-  inventaire: FormControl = new FormControl("", [
-          Validators.min(0)
-  ]);
+    bouteille: any;
 
-  @Input() set uneBouteille(bouteille: any) {
-    this.bouteille = bouteille
-    if (bouteille) {
-      this.inventaire.setValue(bouteille.inventaire);
-      this.inventaire.valueChanges.subscribe(
-        valeur => {
-          console.log(valeur);
-          if(!valeur){
-            return
-          }
-          this.enregistrerNouvelInventaire()})
+    // Propriété permettant de rapidement savoir si la bouteille est en stock ou non
+    enStock: boolean = true;
+
+    inventaire: FormControl = new FormControl("", [
+        Validators.min(0)
+    ]);
+
+    @Input() set uneBouteille(bouteille: any) {
+        this.bouteille = bouteille
+        if (bouteille) {
+            this.inventaire.setValue(bouteille.inventaire);
+            this.inventaire.valueChanges
+                .pipe(startWith(undefined), pairwise())
+                .subscribe(valeur => {
+                        console.log(valeur);
+
+                        if (valeur < 0) {
+                            this.inventaire.setValue(0);
+                            return;
+                        }
+
+                        this.enregistrerNouvelInventaire();
+                    }
+                )
+        }
+
+    };
+
+    constructor(private servBouteilleDeVin: BouteilleDeVinService, public formModifierBouteille: MatDialog) { }
+
+    ngOnInit(): void {
+
     }
 
-};
 
-  constructor(private servBouteilleDeVin:BouteilleDeVinService, public formModifierBouteille: MatDialog) { }
+    augmenter() {
 
-  ngOnInit(): void {
+        this.inventaire.setValue(this.inventaire.value + 1);
 
-  }
-
-
-  augmenter() {
-
-    this.inventaire.setValue(this.inventaire.value + 1);
-
-    //this.enregistrerNouvelInventaire();
-
-
-  }
-
-  diminuer() {
-
-    if(this.inventaire.value - 1 < 0) {
-      this.inventaire.setValue(0)
-      return
+        // Si l'inventaire est au moins à 1, ajuster la propriété
+        if (this.inventaire.value > 0) {
+            this.enStock = true;
+        }
     }
 
-    this.inventaire.setValue(this.inventaire.value - 1);
+    diminuer() {
+        if (this.inventaire.value - 1 < 0) {
+            this.inventaire.setValue(0)
+            return;
+        }
 
-  }
+        this.inventaire.setValue(this.inventaire.value - 1);
+
+        // Si l'inventaire tombe a zéro, ajuster la propriété qui permettra de désactiver le bouton "-"
+        if (this.inventaire.value === 0) {
+            this.enStock = false;
+        }
+    }
 
 
-  enregistrerNouvelInventaire() {
-    this.servBouteilleDeVin.modifierInventaireCellierBouteille(this.bouteille.inventaireId, this.inventaire.value)
-          .subscribe()
-  }
+    enregistrerNouvelInventaire() {
+        this.servBouteilleDeVin.modifierInventaireCellierBouteille(this.bouteille.inventaireId, this.inventaire.value)
+            .subscribe()
+    }
 
-  formulaireModification(data:any): void {
-    let formulaire = this.formModifierBouteille.open(ModifierCellierBouteilleComponent, {
-      data
-    });
+    formulaireModification(data: any): void {
+        let formulaire = this.formModifierBouteille.open(ModifierCellierBouteilleComponent, {
+            data
+        });
 
-    formulaire.afterClosed().subscribe(result => {
-      console.log('formulaire rempli')
-    })
-  }
+        formulaire.afterClosed().subscribe(result => {
+            console.log('formulaire rempli')
+        })
+    }
 
 }
