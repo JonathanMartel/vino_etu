ea026294d4a9f77bc902c89a02542daf9b3dfcbb
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-02 14:15:03 -0400

    Ressource BouteilleAchetee complétée

--- app/Http/Resources/BouteilleAcheteeResource.php
+++ app/Http/Resources/BouteilleAcheteeResource.php
@@ -14,6 +14,23 @@ class BouteilleAcheteeResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id"                 => $this->id,
+            "nom"                => $this->nom,
+            "description"        => $this->description,
+            "url_image"          => $this->url_image,
+            "url_achat"          => $this->url_achat,
+            "url_infos"          => $this->url_infos,
+            "millesime"          => $this->millesime,
+            "date_acquisition"   => $this->date_acquisition,
+            "conservation"       => $this->conservation,
+            "notes_personnelles" => $this->notes_personnelles,
+            "format"             => $this->format,
+            "origine"            => $this->origine,
+            /* "categories_id"      => $this->categories_id,
+            "categories"         => $this->categorie->nom, */
+            "created_at"         => $this->created_at,
+            "updated_at"         => $this->updated_at,
+        ];
     }
 }
