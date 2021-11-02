f432859b3c6030aa601cc54331eb0768cf597ca5
Niwark <blackaru@hotmail.com>
2021-11-02 19:41:48 -0400

    refonte liste des vins du cellier et CSS

--- app/Http/Controllers/CellierBouteilleController.php
+++ app/Http/Controllers/CellierBouteilleController.php
@@ -50,10 +50,11 @@ foreach ($cellierBouteillesIDs as $bouteilleID){
             "format" => $bouteille[0]->format,
             "taille" => $bouteille[0]->taille,
             "url_img" => $bouteille[0]->url_img,
+            "url_saq" => $bouteille[0]->url_saq,
             "dataCellier" => $dataCellierBouteillesByIDs,
     ];
 
-    var_dump($cellierBouteillesByIDs);
+    //var_dump($cellierBouteillesByIDs);
 }
 
 
--- app/Models/Bouteille.php
+++ app/Models/Bouteille.php
@@ -60,7 +60,7 @@ class Bouteille extends Model
      */
     public static function getDataBouteilleByID($IDBouteille) {
         return DB::table('bouteilles')
-        ->select('bouteilles.nom as nom','pays', 'formats.nom as format', 'type', 'formats.taille as taille', 'url_img')
+        ->select('bouteilles.nom as nom','pays', 'formats.nom as format', 'type', 'formats.taille as taille', 'url_img', 'description', 'prix_saq', 'code_saq', 'url_saq')
         ->join('types', 'types.id', '=', 'bouteilles.type_id')
         ->join('formats', 'formats.id', '=', 'bouteilles.format_id')
         ->where('bouteilles.id', $IDBouteille)
