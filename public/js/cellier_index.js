ba9d30ed7de29717b820348d8e254fcc3d7ee512
Brian Tan <t.brian3@gmail.com>
2021-11-08 15:57:40 -0500

    CSS listes des celliers mis à jour

--- public/js/cellier_index.js
+++ public/js/cellier_index.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    const celliers = document.querySelectorAll('.cellier');
+
+    celliers.forEach(cellier => {
+
+      cellier.addEventListener('click', (e) => {
+        
+         if(e.target.classList.contains('cellier') || e.target.classList.contains('nom-cellier') || e.target.classList.contains('localisation-cellier')){
+            location.href = cellier.dataset.lien;
+         }
+         
+        
+      })
+    })
   });
