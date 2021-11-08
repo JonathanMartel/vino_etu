d4745e27cb41a3db71c7e4eb9eaebfd10c778057
Carolina Rojas <carorode77@gmail.com>
2021-11-08 09:41:11 -0500

    ajustement pour enlever pasport

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use Hash;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 
