936dd162f8fa48d68dd80ff22231b018265e2c75
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-11 10:58:21 -0500

    Écriture du AtuhGuard complétée

--- resources/frontend/src/app/services/auth.guard.ts
+++ resources/frontend/src/app/services/auth.guard.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    RouterStateSnapshot,
+    UrlTree,
+    Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from "@services/auth.service"
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    )
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+        estAuth = this.authService.estAuthentifie();
+
+        if(!estAuth) {
+            this.router.navigate(['/connection']);
+        }
+
+        return estAuth;
+    }
 }
