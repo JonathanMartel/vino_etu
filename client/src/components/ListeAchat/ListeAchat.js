2a6e2ea12563b028fa4378444e8d2b5f3ae65e31
Diana Quiroz <dquirozvillanes@yahoo.es>
2021-11-11 21:37:36 -0500

    liste achat

--- client/src/components/ListeAchat/ListeAchat.js
+++ client/src/components/ListeAchat/ListeAchat.js
@@ -3,7 +3,8 @@ import './ListeAchat.css';
 import { DataGrid } from '@mui/x-data-grid';  //import { DataGrid } from '@mui/x-data-grid/index-cjs';
 import Button from '@mui/material/Button';
 import { Box } from "@mui/system";
-import { DataGrid } from '@mui/x-data-grid/index-cjs';
+import { TextField } from "@mui/material";
+//import { DataGrid } from '@mui/x-data-grid/index-cjs';
 
 export default class ListeAchat extends React.Component {
   constructor(props) {
@@ -12,31 +13,46 @@ export default class ListeAchat extends React.Component {
     this.state = {
       items: [],
       itemsSelected: [],
-      bouteilles: [],
-      listeAchat: false
+      bouteilles: []['id', 'millesime', 'quantite'],
+      listeAchat: false,
+      titre: "",
+      idListeAchat: undefined
     }
-
 
     this.fetchBouteilles = this.fetchBouteilles.bind(this);
     this.creerListeAchat = this.creerListeAchat.bind(this);
     this.fetchListeAchat = this.fetchListeAchat.bind(this);
+    this.effacerListe = this.effacerListe.bind(this);
   }
 
   componentDidMount() {
     if (!this.props.estConnecte) {
       return this.props.history.push("/connexion");
     }
-
+    
     this.fetchListeAchat();
-    if (!this.state.listeAchat) {
-      this.fetchBouteilles();
-    }
-    //this.fetchBouteilles();
+    console.log('this.state.listeAchat: ', this.state.listeAchat);
   }
 
   componentDidUpdate() {
     console.log("kkk", this.state.itemsSelected);
-  }
+    
+    this.state.itemsSelected
+            .map((item) => {
+
+                this.state.items.map(x => {
+                    if (x.id === item) {
+                        console.log("x: ", x);
+                        this.setState({bouteilles: [[x.id, x.millesime, x.quantite]]});
+                        //console.log("this.state.bouteilles: ", this.state.bouteilles);
+
+                        //CODE ICI POUR PRÉPARER À SEND LE FETCH
+                        // var item est égale à l'object de la bouteille sélectionnée avec toutes ses infos.
+                    }
+                });
+            })
+    
+  } 
 
 
   fetchListeAchat() {
@@ -51,12 +67,23 @@ export default class ListeAchat extends React.Component {
       .then((donnees) => {
         this.setState({ items: donnees.data });
         this.setState({ listeAchat: true });
+        //console.log("Éxiste listeAchat ? : ", this.state.listeAchat);
       });
+    
+      console.log('this.state.items: ', this.state.items);
+      console.log("Éxiste listeAchat ? : ", this.state.listeAchat);
+
+      if (!this.state.listeAchat) {
+        this.fetchBouteilles();
+        this.setState({titre: "Inventaire des bouteilles"});
+      } else {
+        this.setState({titre: "Liste d'achat"});
+      }
   }
 
   fetchBouteilles() {
     //Ici on doit mettre le nouveau fetch pour avoir la liste de vinos de tous nos celliers avec la quantite, pour savoir sin on doit acheter
-    fetch('https://rmpdwebservices.ca/webservice/php/celliers/' + this.props.id_usager + '/bouteilles', {
+    fetch('https://rmpdwebservices.ca/webservice/php/bouteilles/usager/' + this.props.id_usager, {
       method: 'GET',
       headers: new Headers({
         'Content-Type': 'application/json',
@@ -72,12 +99,22 @@ export default class ListeAchat extends React.Component {
   creerListeAchat() {
     console.log("Créer liste d'achat");
     console.log("Colonnes séléctionnées: ", this.state.itemsSelected);
+    /*
+    this.state.itemsSelected
+            .map((item) => {
 
-
-    //recorrer la lista para llebar millesime y la cantidad
-    /*for (let i = 0; i < this.state.itemsSelected.length; i++) {
-      console.log('Item: ', this.state.itemsSelected[i]);
-    }*/
+                this.state.items.map(x => {
+                    if (x.id === item) {
+                        console.log("x: ", x);
+                        this.setState({bouteilles: x})
+                        this.setState({bouteilles: [x.id, x.millesime, x.qantite]});
+                        //CODE ICI POUR PRÉPARER À SEND LE FETCH
+                        // var item est égale à l'object de la bouteille sélectionnée avec toutes ses infos.
+                    }
+                });
+            })
+    */
+    console.log("this.state.bouteilles: ", this.state.bouteilles);
     /*
     bouteilles = this.state.itemsSelected
                             .map((item, index) => {
@@ -112,8 +149,37 @@ export default class ListeAchat extends React.Component {
 
   }
 
+  effacerListe() {
+
+    this.state.items.map(x => {
+        console.log("x: ", x.id);
+        this.setState({idListeAchat: x.id});
+    });
+
+    console.log("Liste d'achat: ", this.state.idListeAchat);
+    /*
+    const postMethod = {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json',
+            authorization: 'Basic ' + btoa('vino:vino')
+        },
+        body: JSON.stringify(donnes)
+    };
+
+    fetch('https://rmpdwebservices.ca/webservice/php/listeachat/', postMethod)
+        .then((reponse) => reponse.json())
+        .then((donnees) => {
+            if (donnees.data) return this.props.history.push("/listeachat");
+        });
+    */
+  }
+
   render() {
-    console.log("items: ", this.state.items);
+    //console.log("items: ", this.state.items);
+
+    console.log("Éxiste listeAchat ? : ", this.state.listeAchat);
+    
 
     const colonnes = [
       //{ field: 'bouteille_id', headerName: 'ID', width: 90, type: 'number' },
@@ -130,27 +196,22 @@ export default class ListeAchat extends React.Component {
         margin: "0 auto", marginTop: "20vh", color: "white",
       }} >
 
-        <span>Liste d'achat</span>
+        <span> {this.state.titre} </span>
         <div className="liste_achat_rows" style={{ height: 400, width: '100%' }}>
           <DataGrid sx={{ color: "white !important", }} rows={this.state.items} columns={colonnes}
             pageSize={5} rowsPerPageOptions={[5]} checkboxSelection disableSelectionOnClick
             onSelectionModelChange={itm => this.setState({ itemsSelected: itm })}
-          //onSelectionModelChange={itm => console.log(this.state.items.bouteille_id)}
           />
         </div>
 
-        {/* 
-                <div className="liste_achat_rows"  style={{ height: 400, width: '100%' }}>
-                    <DataGrid sx={{ color: "white !important", }} rows={this.state.itemsSelected} columns={colonnes}
-                        pageSize={5} rowsPerPageOptions={[5]} checkboxSelection disableSelectionOnClick 
-                    />
-                </div>
-              */}
+        
+        <label className="liste_achat_check" onClick={(e) => this.effacerListe()} >Effacer liste d'achat <input type="checkbox" /> </label>
 
-        <label for="scales">mmmm</label>
-        <input type="checkbox" />
+       {/* <TextField label="Effacer liste d'achat" variant="outlined" value="Effacer liste d'achat" type="checkbox" style={{ color: 'white', width: '20%' }}
+                    onChange={evt => this.setState({ emplacement: evt.target.value })} /> */}
+
         <Button className="button" type="button" onClick={(e) => this.creerListeAchat()}> Créer Liste </Button>
       </Box>
     );
   }
-}
+} 
