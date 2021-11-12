48d5da540811427e16a3c70e44a3bb09a2069872
dgtlmlta <nicolas@digitalmilitia.net>
2021-11-11 20:37:05 -0500

    Pull pour fixs css

--- resources/frontend/src/app/services/auth.service.ts
+++ resources/frontend/src/app/services/auth.service.ts
@@ -12,9 +12,9 @@ export class AuthService {
     _utilisateurToken!: any;
 
 
-    private url: string = "http://127.0.0.1:8000/api";
-    //private url: string = "http://kalimotxo-vino.akira.dev/api";
-    // private url: string = new URL(window.location.href).origin + "/api";
+    // private url: string = "http://127.0.0.1:8000/api";
+    // private url: string = "http://kalimotxo-vino.akira.dev/api";
+    private url: string = new URL(window.location.href).origin + "/api";
 
     constructor(
         private http: HttpClient
