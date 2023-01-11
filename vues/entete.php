b3381d077b8ed7e5dc46ddb503803bbee4b1097e
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-11 16:25:04 -0500

    integration css

--- vues/entete.php
+++ vues/entete.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<title>Un petit verre de vino</title>
 
-		<meta charset="utf-8">
-		<meta http-equiv="cache-control" content="no-cache">
-		<meta name="viewport" content="width=device-width, minimum-scale=0.5, initial-scale=1.0, user-scalable=yes">
+<head>
+	<title>Un petit verre de vino</title>
 
-		<meta name="description" content="Un petit verre de vino">
-		<meta name="author" content="Jonathan Martel (jmartel@cmaisonneuve.qc.ca)">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=0.5, initial-scale=1.0, user-scalable=yes">
 
-		<link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
-		<link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
-		<link rel="stylesheet" href="./css/main.css" type="text/css" media="screen">
-		<base href="<?php echo BASEURL; ?>">
-		<!--<script src="./js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>-->
-		<script src="./js/plugins.js"></script>
-		<script src="./js/main.js"></script>
-	</head>
-	<body >
-		<header>
-			<h1>Un petit verre de vino ?</h1>
-			<nav>
-				<ul>
-					<li><a href="?requete=accueil">Mon cellier</a></li>
-					<li><a href="?requete=ajouterNouvelleBouteilleCellier">Ajouter une bouteille au cellier</a></li>
-				</ul>
-			</nav>
-		</header>
-		<main>
-			
+	<meta name="description" content="Un petit verre de vino">
+	<meta name="author" content="Jonathan Martel (jmartel@cmaisonneuve.qc.ca)">
+
+	<link rel="stylesheet" href="./css/main.css">
+
+	<!--<script src="./js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		 <script src="./js/plugins.js"></script>
+		<script src="./js/main.js"></script>-->
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<a href="#" class="logo"><img src="./img/WineBottle.svg" alt="logo vino"></a>
+			<div class="link">
+				<a href="">Mon cellier</a>
+				<a href="">Ajouter une bouteille au cellier</a>
+				<a href="">Déconnexion</a>
+			</div>
+		</nav>
+	</header>
