df45a120a019e46819ee29ca3abe7b3ee664b11c
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-11 16:24:37 -0500

    integration css

--- vues/ajouter.php
+++ vues/ajouter.php
@@ -1,20 +1,33 @@
-<div class="ajouter">
 
-    <div class="nouvelleBouteille" vertical layout>
-        Recherche : <input type="text" name="nom_bouteille">
-        <ul class="listeAutoComplete">
+<section class="newletter formCo enchereForm">
 
-        </ul>
-            <div >
-                <p>Nom : <span data-id="" class="nom_bouteille"></span></p>
-                <p>Millesime : <input name="millesime"></p>
-                <p>Quantite : <input name="quantite" value="1"></p>
-                <p>Date achat : <input name="date_achat"></p>
-                <p>Prix : <input name="prix"></p>
-                <p>Garde : <input name="garde_jusqua"></p>
-                <p>Notes <input name="notes"></p>
-            </div>
-            <button name="ajouterBouteilleCellier">Ajouter la bouteille (champs tous obligatoires)</button>
-        </div>
-    </div>
-</div>
+    <form class='inscCo' data-js-form action="" enctype="multipart/form-data" method="post">
+
+        <h5 class="form-title">Ajouter une bouteille</h5><br>
+
+        Nom <input type="text" name="" required>
+        Millesime <input type="text" name="millesime" required>
+        quantite <input type="text" name="quantite" value='1' required>
+        Date achat  <input type="text" name="Date achat " required>
+        Garde <input type="text" name="Garde" required>
+
+        Cellier <select name="" class="trier select" value="trier par" required>
+            <option ></option>
+            <option value="t">cuisine</option>
+            <option value="">chalet</option>
+            <option value="">spa</option>
+            
+        </select><br>
+
+        Image(s) <input name="img_nom" type="file" accept=".jpg, .jpeg, .png" required>
+        Prix <input type="number" name="" required>
+        Notes <input type="textarea" name="Notes" required>
+ 
+
+        <button name="ajouterBouteilleCellier">ajouter</button><br>
+        <span id="erreur" style="color: red;"></span>
+
+    </form>
+
+
+</section>
