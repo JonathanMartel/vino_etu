6affa7bb6761a951b6adfe47d57cbd8095a29cec
saidboude <e9875172@cmaisonneuve.qc.ca>
2023-01-10 21:59:15 -0500

    Fixe BASE_URL

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@
 
 //const BaseURL = "https://jmartel.webdev.cmaisonneuve.qc.ca/n61/vino/";
 //const BaseURL = document.baseURI;
-const BaseURL = '127.0.0.1/vino_etu'
 console.log(BaseURL);
 window.addEventListener('load', function() {
     console.log("load");
