e397cd51d69d9b6e7ef0d92eb9d6344b57c9237b
saidboude <e9875172@cmaisonneuve.qc.ca>
2023-01-10 22:54:41 -0500

    Mettre la config de la base de donnee locale

---
+++ datconf.model.php
@@ -0,0 +1,10 @@
+<?php
+	// Remplir et renommer dataconf.php
+	define("BASEURL", "/vino_etu");
+
+	define('HOST', '127.0.0.1');
+	define('USER', 'root');
+	define('PASSWORD', '');
+	define('DATABASE', 'vinodb');
+?>
+
