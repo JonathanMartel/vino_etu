31ad4e66bddcb1ec9bbfeef315f196f15c6fdf73
RBC <renaud_b-c@hotmail.com>
2023-01-17 22:22:31 -0500

    Ajout de la vue accueil

---
+++ vues/accueil.php
@@ -0,0 +1,6 @@
+
+
+<section class="bg-img">
+    <p class="bienvenue">Bienvenue dans notre nouveau site Web de cave à vin. C'est un endroit où vous pourrez explorer le monde des grands vins, trouver l'inspiration et profiter de nos fonctionnalités incroyables de nos spécialistes.</p>
+</section>
+
