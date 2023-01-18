fe7250c7622bc93e707e39d4928458b3264bba73
RBC <renaud_b-c@hotmail.com>
2023-01-17 22:23:20 -0500

    Ajout de la vue register

---
+++ vues/register.php
@@ -0,0 +1,24 @@
+<div class="register">
+
+<!--     <h1>Création de compte</h1>
+   <form method="post">
+        <p>Nom : <input name="nom"></p>
+        <p>Email : <input name="email"></p>
+        <p>Mot de passe : <input name="mdp"></p>
+
+    </form> 
+    <button>Créer</button>
+ -->
+
+    <div class="form-style-8">
+  <h2>Création de compte</h2>
+  <form action="?requete=register" method="post">
+    <input type="text" name="nom" placeholder="Nom" />
+    <input type="email" name="email" placeholder="Email" />
+    <input type="text" name="mdp" placeholder="Mot de passe" />
+    <input type="submit" value="Créé" class="top"/>
+  </form>
+</div>
+
+
+</div>
