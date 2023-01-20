b285cbc40e65343ba5655a69e0b7e356b85dd9b5
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2023-01-20 14:26:56 -0500

    modification dashboard avec admin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,16 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <!-- admin seulement -->
-                    @can('super-admin')
-                    <x-nav-link :href="route('question.1')" :active="request()->routeIs('question.1')">
-                        {{ __('question.1') }}
+                    @if(Auth::user()->is_admin)
+                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
+                        {{ __('Admin Dashboard') }}
                     </x-nav-link>
-                    @endcan
-                    <!-- Aller sur**** -->
-                    
-                    <!-- Aller sur catelogue bouteille liste -->
-                   
-
+                    @endif
                 </div>
             </div>
 
@@ -35,7 +30,6 @@
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
