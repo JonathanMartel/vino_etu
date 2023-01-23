2dbd9ec3886967600d0157ede0db5c791fbe0f9f
RBC <renaud_b-c@hotmail.com>
2023-01-23 18:46:42 -0500

    Ajout de la vues ajoutcellier.php

---
+++ vues/ajoutcellier.php
@@ -0,0 +1,16 @@
+<div class="ajouter">
+
+    
+    <div class="form-style-8">
+        
+        <h1>Ajout d'un nouveau cellier</h1>
+        <form action="?requete=ajoutercellier" method="post">
+            <input type="text" name="nom" placeholder="Nom du Cellier" />
+            <input type="text" name="lieu" placeholder="Lieu du Cellier" />
+
+            <input type="submit" value="Ajouter le Cellier" class="top"/>
+        </form>
+
+    </div>
+
+</div>
