34b4575bc087f9b2782ab5ae40494c1c39dd2688
RBC <renaud_b-c@hotmail.com>
2023-01-31 20:45:44 -0500

    Ajout des lien ajouter priver

--- vues/cellier.php
+++ vues/cellier.php
@@ -3,6 +3,8 @@
 <h1>Nom du Cellier: <?= $datacell[0]['nom']?></h1>
 <!-- <p><a href="?requete=ajouterNouvelleBouteilleCellierPrive">Ajouter une bouteille au cellier privé</a></p> -->
 <!-- <p><a href="?requete=ajouterNouvelleBouteilleCellier" class="btnlogin button-28">Ajouter une bouteille au cellier de la SAQ</a></p> -->
+<h3><a href="?requete=ajouterNouvelleBouteilleCellierPrive&id=<?= $datacell[0]['id']?>" class="btnlogin button-28">Ajouter une bouteille privé</a></h3>
+      <h3><a href="?requete=ajouterNouvelleBouteilleCellier" class="btnlogin button-28">Ajouter une bouteille SAQ</a></h3>
     
 
 <?php
