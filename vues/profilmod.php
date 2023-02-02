b477dc1552b694b422146b0609b566f7bbeb1090
Dev2023 <e2194608@cmaisonneuve.qc.ca>
2023-02-02 16:30:33 -0500

    Retour du trvail fait sur la vue de modifcation de profil

--- vues/profilmod.php
+++ vues/profilmod.php
@@ -2,15 +2,7 @@
 <div class="form-style-8">
 	<h2>Modifier Vos information</h2>
 	<form action="?requete=profilmod" method="post">
-  <label for="nom">Nom</label>
-    <input type="text" name="nom" id="nom" value="<?= $_SESSION['usager'][0]['nom'] ?>" />
-    <label for="email">Courriel</label>
-    <input type="email" name="email" id="email" value="<?= $_SESSION['usager'][0]['email'] ?>" />
-    <label for="mdp" >Nouveau mot de passe</label>
-    <input type="password" name="mdp" id="mdp" />
-    <label for="mdpConfirm">Confirmer mot de passe</label>
-    <input type="password" name="mdpConfirm" id="mdpConfirm">
-
+    <input type="text" name="nom" value="<?= $_SESSION['usager'][0]['nom'] ?>" />
     <button type="submit" class="top button-28">Modifier</button>
   </form>
 </div>
