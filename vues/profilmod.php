f8db583b4eba5a4ea3e9652cddf9c9246d6b880c
Dev2023 <e2194608@cmaisonneuve.qc.ca>
2023-02-04 12:21:49 -0500

    Ajout de span d'erreur sur la vue profilmod

--- vues/profilmod.php
+++ vues/profilmod.php
@@ -1,7 +1,8 @@
 
 <div class="form-style-8">
 	<h2>Modifier Vos information</h2>
-	<form action="?requete=profilmod" method="post">
+	<form action="?requete=profilmod" method="post" id="modProfilForm">
+    <span id="nom" class="error-message"></span>
     <input type="text" name="nom" value="<?= $_SESSION['usager'][0]['nom'] ?>" />
     <button type="submit" class="top button-28">Modifier</button>
   </form>
