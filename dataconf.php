1aa81d9a4844c0e98df5dd50eb8b6c675c2dd537
saidboude <e9875172@cmaisonneuve.qc.ca>
2023-02-06 18:28:48 -0500

    modification BASEURL POUR HEBERGEUR WEB

--- dataconf.php
+++ dataconf.php
@@ -2,18 +2,18 @@
 	// Remplir et renommer dataconf.php
 
 
-	define("BASEURL", "http://localhost:8080/vino_etu/");
+	//define("BASEURL", "http://localhost/vino_etu/");
 	//define("BASEURL", "https://vino-etu.000webhostapp.com/");
 
-	define('HOST', 'localhost');
-	define('USER', 'root');
-	define('PASSWORD', '');
-	define('DATABASE', 'vinodbcurrent2');
+	/*define('HOST', 'localhost');
+	//define('USER', 'root');
+	//define('PASSWORD', '');
+	//define('DATABASE', 'vinodbcurrent2');*/
 	
 	//Herbergeur
-	//define("BASEURL", "https://vino-etu.000webhostapp.com/");
-/* 	define('HOST', 'localhost');
+	define("BASEURL", "https://vino-etu.000webhostapp.com/");
+    define('HOST', 'localhost');
 	define('USER', 'id20171121_root');
 	define('PASSWORD', 'T$2=~NU#_g76>nKu');
-	define('DATABASE', 'id20171121_vinodb'); */
+	define('DATABASE', 'id20171121_vinodb');
 ?>
